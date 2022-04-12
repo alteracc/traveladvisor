941c3c3f50e6a0747856a487afe18e0aca8e7d5e
Pandit <skpandit@lowes.com>
2022-04-12 15:43:54 +0530

    initial travel advisor

---
+++ src/api/index.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+// 5cb1896f9emsh66873c6c69557d6p1320f6jsnd0749f884d32
+
+// const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
+
+export const getPlacesData = async (type, sw, ne) => {
+    try{
+        const {data : { data }} = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,{
+            params: {
+              bl_latitude: sw.lat,
+              tr_latitude: ne.lat,
+              bl_longitude: sw.lng,
+              tr_longitude: ne.lng,
+            },
+            headers: {
+                'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
+                'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY
+            }})
+        return data;
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+export const getWeatherData = async (lat, lng) => {
+    try {
+      if (lat && lng) {
+        const { data } = await axios.get('https://community-open-weather-map.p.rapidapi.com/find', {
+          params: { lat, lon: lng },
+          headers: {
+            'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
+            'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+          },
+        });
+  
+        return data;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
